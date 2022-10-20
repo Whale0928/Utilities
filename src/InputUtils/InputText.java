808746e23f30c6ab6c5d62854a262cef00e32af8
KIM HYUN GI <rlagusrl928@gmail.com>
2022-10-20 21:32:11 +0900

    최초 커밋

---
+++ src/InputUtils/InputText.java
@@ -0,0 +1,49 @@
+package InputUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * 텍스트 파일을 입력받고 여러 종류별로 반환하는 메서드
+ */
+public class InputText {
+
+
+    /**
+     * @param filePath 파일경로가 저장되있어야 한다.
+     * @return 한줄로 정렬된 문자열
+     */
+    public String inputString(String filePath){
+        String line = "";
+        StringBuilder sb = new StringBuilder();
+        File file = new File(filePath);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            System.out.println("File Input Fail!!!");
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        InputText in = new InputText();
+        String str =  in.inputString("/Users/deadwhale/Downloads/QueryTxt.txt");
+        str = str.replace(",","");
+
+        String[] arr = str.split("");
+        for (String s : arr){
+            System.out.println(s);
+        }
+
+
+    }
+
+
+}
+
