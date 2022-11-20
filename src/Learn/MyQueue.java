41a7995efca60bb2878544fee4f1d72ed9a3a448
whale0928 <rlagusrl928@gmail.com>
2022-11-20 22:05:42 +0900

    feat : 큐 구현
    - 큐를 직접 구현해보았다.

---
+++ src/Learn/MyQueue.java
@@ -0,0 +1,44 @@
+package Learn;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyQueue<T> {
+    private final ArrayList<T> queue = new ArrayList<T>();
+
+    /**큐에 데이터를 삽입
+     * Enqueue.
+     *
+     * @param item the item
+     */
+    public void enqueue(T item) {
+        queue.add(item);
+    }
+
+    /**큐에서 데이터를 꺼낸다 없을 경우에는 0을 반환 있는 경우 항상 가장 앞의 값만 반환
+     * Dequeue t.
+     *
+     * @return the t
+     */
+    public T dequeue() {
+        if (queue.isEmpty()) {
+            return null;
+        }
+        return queue.remove(0);
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        MyQueue<Integer> mq = new MyQueue<Integer>();
+        mq.enqueue(2);
+        mq.enqueue(1);
+        mq.enqueue(3);
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
+    }
+}
